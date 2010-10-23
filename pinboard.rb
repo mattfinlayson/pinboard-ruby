349dcdd0c3611d4c698e511f05d365d32d6e6e9c
Akash Manohar J <akash@akash.im>
2010-10-23 18:47:55 +0530

    /posts calls done
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- pinboard.rb
+++ pinboard.rb
@@ -10,42 +10,56 @@ class Pinboard
   end
 
   def getPosts(options={})
-    apiData = {:basic_auth=>@auth}
+    apiData = { :basic_auth=>@auth }
     apiData.merge!(options)
     self.class.get('/posts/get', apiData)
   end
 
-  def recentPosts()
-    #TODO
+  def recentPosts(options={})
+    apiData = { :basic_auth=>@auth }
+    apiData.merge!(options)
+    self.class.get('/posts/recent', apiData)
   end
 
   def addPost(u, d, options={})
-    apiQuery = {:url=>u, :description=>d}
-    apiData = {
-      :basic_auth=>@auth, :query=>apiQuery
-    }
-    
+    apiQuery = {:url=>u, :description=>d}.merge(options)
+    apiData = { :basic_auth=>@auth, :query=>apiQuery }
     self.class.get('/posts/add', apiData)
   end
 
-  def deletePost()
-    #TODO
+  def deletePost(url)
+    apiQuery = { :url=>url }
+    apiData = { :basic_auth=>@auth, :query=>apiQuery }
+    self.class.get('/posts/delete', apiData)
   end
 
-  def getDates()
-    #TODO
+  def getDates(tag='')
+    if(tag.strip.empty?)
+      apiData = { :basic_auth=>@auth, :query=>{:tag=>tag} }
+    else
+      apiData = { :basic_auth=>@auth }
+    end
+
+    self.class.get('/posts/delete', apiData)
   end
 
   def allPosts()
     #TODO
   end
     
-  def hashAll
-    #TODO
+  def getHashes
+    apiData = { :basic_auth=>@auth, :query=>{:hashes} }
+    self.class.get("/posts/all?hashes", apiData)
   end
   
-  def suggestPosts()
-    #TODO
+  def suggestPosts(url='')
+    if(url.strip.empty?)
+      apiData = { :basic_auth=>@auth, :query=>{:url=>url} }
+    else
+      apiData = { :basic_auth=>@auth }
+    end
+
+    self.class.get('/posts/suggest', apiData)
   end
 
   def getTags()
@@ -80,8 +94,7 @@ puts "and the password: "
 p = gets.chomp
 puts "Let me process"
 pb = Pinboard.new(:username=>u, :password=>p)
-puts pb.getPosts()
 puts "******************"
-puts "**** ADD POST ****"
+puts "**** NEW POSTS ****"
 puts "******************"
-puts pb.addPost('http://google.com', 'google')
+puts pb.getHashes
